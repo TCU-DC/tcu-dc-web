0778b1877b54d69927667b19036d2df6f7287955
Shotaro K <git@shotaro.jp>
2024-06-29 23:12:35 +0900

    [wip] 記事一覧ページネーション

--- src/utils/microcms/getContents.ts
+++ src/utils/microcms/getContents.ts
@@ -17,6 +17,7 @@ export const getGroup = (queries?: MicroCMSQueries) => (contentId: string) =>
     contentId,
     queries,
   });
+
 export const getMembers = (queries?: MicroCMSQueries) =>
   apiClient.getList<Member>({ endpoint: "members", queries });
 export const getMember = (queries?: MicroCMSQueries) => (contentId: string) =>
@@ -36,6 +37,7 @@ export const getPage = (queries?: MicroCMSQueries) => (contentId: string) =>
   });
 export const getPageIds = () =>
   apiClient.getAllContentIds({ endpoint: "pages" });
+
 export const getPosts = (queries?: MicroCMSQueries) =>
   apiClient.getList<Post>({ endpoint: "posts", queries });
 export const getPost = (queries?: MicroCMSQueries) => (contentId: string) =>
@@ -46,15 +48,18 @@ export const getPost = (queries?: MicroCMSQueries) => (contentId: string) =>
   });
 export const getPostIds = () =>
   apiClient.getAllContentIds({ endpoint: "posts" });
+
 export const getPostCategories = (queries?: MicroCMSQueries) =>
-  apiClient.getList<PostCategory>({ endpoint: "post_category", queries });
+  apiClient.getList<PostCategory>({ endpoint: "post_categories", queries });
 export const getPostCategory =
   (queries?: MicroCMSQueries) => (contentId: string) =>
     apiClient.getListDetail<PostCategory>({
-      endpoint: "post_category",
+      endpoint: "post_categories",
       contentId,
       queries,
     });
+export const getPostCategoryIds = () =>
+  apiClient.getAllContentIds({ endpoint: "post_categories" });
 
 export const getTopSetting = (queries?: MicroCMSQueries) =>
   apiClient.getListDetail<Top>({
@@ -78,7 +83,7 @@ export async function getPostsPaginated(
   currentPage: number = 1,
   limit: number = 10,
   categoryId?: string,
-): Promise<{ posts: MicroCMSListResponse<Post>; maxPage: number }> {
+): Promise<{ posts: MicroCMSListResponse<Post>; pager: number[] }> {
   const posts = await getPosts({
     limit,
     offset: (currentPage - 1) * limit,
@@ -86,6 +91,8 @@ export async function getPostsPaginated(
   });
   // 最大のページ数を計算
   const maxPage = Math.ceil(posts.totalCount / limit);
+  // ページャーの配列を作成
+  const pager = Array.from({ length: maxPage }, (_, i) => i + 1);
 
-  return { posts, maxPage };
+  return { posts, pager };
 }
