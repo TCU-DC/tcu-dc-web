e9474b27894bedde91cdb3ed23aae8036eb686ce
Shotaro K <git@shotaro.jp>
2024-11-17 08:32:26 +0900

    [add] トップページ作成

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import Top from "@/components/Top";
+import type { Config } from "@/types/microcms/config";
+import type { Post } from "@/types/microcms/post";
+import { getConfig, getPosts } from "@/utils/microcms/getContents";
+import type { MicroCMSListResponse } from "microcms-js-sdk";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+
+export const dynamicParams = false;
+
+export async function generateMetadata(): Promise<Metadata> {
+  const config: Config = await getConfig();
+  return {
+    description: config.description,
+  };
+}
+
+export default async function Page() {
+  const NEWS_LIMIT = 2; // トップページに表示する記事の数
+
+  const config: Config = await getConfig().catch(() => notFound());
+  const posts: MicroCMSListResponse<Post> = await getPosts({
+    limit: NEWS_LIMIT,
+  })().catch(() => notFound());
+  return <Top config={config} posts={posts}></Top>;
+}
