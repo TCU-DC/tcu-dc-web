ec6c21e9a88ee59086cea45598d53da924b9ca9e
Shotaro K <git@shotaro.jp>
2024-11-26 23:49:12 +0900

    [upd] page コンポーネント デザインスマホ対応

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -4,8 +4,8 @@ import type { MicroCMSContentId, MicroCMSDate } from "microcms-js-sdk";
 
 function Page({ page }: { page: Page & MicroCMSContentId & MicroCMSDate }) {
   return (
-    <div className="bg-zinc-100 pb-16 pt-32">
-      <div className="mx-auto w-[848px] rounded bg-white p-20">
+    <div className="bg-zinc-100 pb-16 pt-8 md:pt-20 lg:pt-32">
+      <div className="mx-8 rounded bg-white p-8 sm:p-16 md:mx-20 md:p-20 lg:mx-auto lg:w-[848px]">
         <Heading heading={page.title ?? ""} level="h1"></Heading>
         <article
           className="prose mt-10 !max-w-none"
