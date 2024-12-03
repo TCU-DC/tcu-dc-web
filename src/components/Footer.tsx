81ff342f4a58c8c9eed6abcf65843d3957f79471
Shotaro K <git@shotaro.jp>
2024-12-03 11:46:19 +0900

    [upd] フッター デザイン変更 リンク集追加

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,36 @@
-import type { Config } from "@/types/microcms/config";
+import type {
+  Config,
+  ConfigCustomFieldFooterLinks,
+} from "@/types/microcms/config";
+import { normalizedCustomFieldLink } from "@/utils/microcms/normalizedCustomFieldLink";
 import Image from "next/image";
 import Link from "next/link";
 
 const Footer = (config: Config) => {
+  const footerLink = (links?: ConfigCustomFieldFooterLinks) => (
+    <div className="w-1/2 pb-6 pr-6 md:w-1/4">
+      <ul className="flex flex-col gap-2 text-sm sm:text-base">
+        <li className="font-bold">{links?.title}</li>
+        <ul className="flex flex-col gap-1">
+          {links?.links &&
+            links.links.map((link) => (
+              <li key={link.fieldId}>
+                <Link
+                  href={normalizedCustomFieldLink(link).link ?? ""}
+                  className="line-clamp-1"
+                >
+                  {normalizedCustomFieldLink(link).title ?? ""}
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </ul>
+    </div>
+  );
+
   return (
     <div className="bg-black">
-      <div className="py-6 pl-8 text-zinc-400">
+      <div className="px-2 py-6 text-zinc-400 sm:px-8">
         <div className="flex items-center">
           <Link href="/">
             <Image
@@ -16,21 +41,20 @@ const Footer = (config: Config) => {
               height="48"
             />
           </Link>
-          <div className="mx-9 h-4 w-0.5 bg-white"></div>
-          <Link href="https://x.com/tcu_dc" target="_blank">
-            <Image
-              className="transition duration-500 hover:opacity-50"
-              src="/x_logo_white.svg"
-              alt="X（SNS）"
-              width="40"
-              height="36"
-            />
-          </Link>
         </div>
-        <div className="flex items-center pt-6">
-          <p>©︎2014-2023 東京都市大学デジタルコンテンツ研究会</p>
-          <div className="mx-3 h-3 w-px bg-zinc-400"></div>
-          <Link href="/privacy-policy">プライバシーポリシー</Link>
+        <div>
+          <div className="flwx-row flex flex-wrap pt-6 lg:w-[1024px] hover:[&_a]:underline">
+            {footerLink(config.footerLinks01)}
+            {footerLink(config.footerLinks02)}
+            {footerLink(config.footerLinks03)}
+            {footerLink(config.footerLinks04)}
+          </div>
+          <div className="text-sm sm:text-base">
+            <p>
+              ©︎2014-{new Date().getFullYear()}{" "}
+              東京都市大学デジタルコンテンツ研究会
+            </p>
+          </div>
         </div>
       </div>
     </div>
