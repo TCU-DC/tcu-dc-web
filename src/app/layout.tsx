64b3099419c86e9b1e81e26dc22b01576cd8f96b
Shotaro K <git@shotaro.jp>
2024-12-04 10:24:19 +0900

    [upd] viewport 追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className={zenKakuGothicNew.className}>{children}</body>
     </html>
   );
